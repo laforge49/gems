b5e822f70980b699e048feae2aafde83b81bd4bf
Bill La Forge <laforge49@gmail.com>
2022-04-01 15:08:52 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject asynchronous-entities "0.1.0-SNAPSHOT"
+(defproject gems "0.1.0-SNAPSHOT"
   :description "An application framework with support for interchanges, roles, order and aggregation."
-  :url "https://github.com/laforge49/asynchronous-entities"
+  :url "https://github.com/laforge49/gems"
   :license {:name "APACHE LICENSE, VERSION 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
