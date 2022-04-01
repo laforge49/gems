66f93942042971655c4ee59b032e35a3fd8e44eb
Bill La Forge <laforge49@gmail.com>
2022-04-01 15:07:29 -0400

    restore project

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject gems "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns gems.core})
+(defproject asynchronous-entities "0.1.0-SNAPSHOT"
+  :description "An application framework with support for interchanges, roles, order and aggregation."
+  :url "https://github.com/laforge49/asynchronous-entities"
+  :license {:name "APACHE LICENSE, VERSION 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.3.618"]
+                 [tupelo "21.07.08"]]
+  :main ^:skip-aot ae.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
